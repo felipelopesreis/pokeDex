6a6ce58deba4ffa234edefc5982fef652b763d3b
felipelopesreis <felipe.reis@estudante.ifms.edu.br>
2022-12-02 22:48:46 -0400

    Localizando pokemon api

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,32 @@
 import '../css/style.css'
 
+const inputPesquisa = document.querySelector("#inputPesquisa")
+const btnLocalizar = document.querySelector("#btnLocalizar")
+const pokedexDisplay = document.querySelector("#display")
+
+/* Adicionando Eventos */
+
+btnLocalizar.addEventListener('click', async function(){
+    /* Buscar os dados do pokemon na API */
+    const dadosDoPokemon = await localizarPokemon(inputPesquisa.value)
+    /* Criar o Cartyão do Pokemon */
+    criarCartao(dadosDoPokemon)
+})
+
+async function localizarPokemon(termoBusca){
+    const url = `https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+    const response = await fetch(url)
+    const pokemon = await response.json()
+   // console.log(pokemon)
+    return pokemon
+}
+
+function criarCartao(pokemon){
+    const cartaoPokemon = document.createElement('div')
+    cartaoPokemon.className='cartaoPokemon'
+    cartaoPokemon.innerHTML = `
+        <img class="pokemonSprite" src="${pokemon.sprites.front_default}"/>
+        <h2>${pokemon.name}</h2>
+    `
+    pokedexDisplay.appendChild(cartaoPokemon)
+}
